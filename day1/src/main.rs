e933cf3302336ba72f2d5bea0019ed965629e6c2
Anders Åstrand <aastrand@gmail.com>
2020-12-01 19:25:07 +0100

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,67 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn solve1(filename: &str) -> i32 {
+    let contents = lines_from_file(filename);
+    let mut result = 0;
+
+    for left in 0..contents.len() {
+        for right in 0..contents.len() {
+            let l = contents[left].parse::<i32>().unwrap();
+            let r = contents[right].parse::<i32>().unwrap();
+            if l != r && l + r == 2020 {
+                result = l * r
+            }
+        }
+    }
+
+    result
+}
+
+fn solve2(filename: &str) -> i32 {
+    let contents = lines_from_file(filename);
+    let mut result = 0;
+
+    for left in 0..contents.len() {
+        for right in 0..contents.len() {
+            for mid in 0..contents.len() {
+                let l = contents[left].parse::<i32>().unwrap();
+                let r = contents[right].parse::<i32>().unwrap();
+                let m = contents[mid].parse::<i32>().unwrap();
+                if l != r && r != m && l + r + m == 2020 {
+                    result = l * r * m;
+                }
+            }
+        }
+    }
+
+    result
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+
+fn main() {
+    println!("{}", solve1("input.txt"));
+    println!("{}", solve2("input.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(solve1("example.txt"), 514579);
+        assert_eq!(solve2("example.txt"), 241861950);
+    }
+}

fb43d9d8802c06e96af27f134a0f48069c7205b1
Anders Åstrand <aastrand@gmail.com>
2020-12-06 12:27:18 +0100

    Day 6

---
+++ day6/src/main.rs
@@ -0,0 +1,56 @@
+use std::fs;
+
+use std::collections::HashSet;
+
+fn solve1(filename: &str) -> u64 {
+    fs::read_to_string(filename)
+        .unwrap()
+        .split("\n\n")
+        .map(|group| {
+            group
+                .replace("\n", "")
+                .chars()
+                .collect::<HashSet<char>>()
+                .len() as u64
+        })
+        .sum()
+}
+
+fn solve2(filename: &str) -> u64 {
+    fs::read_to_string(filename)
+        .unwrap()
+        .split("\n\n")
+        .map(|group| {
+            let mut sets = group
+                .split("\n")
+                .map(|set| set.chars().collect::<HashSet<char>>());
+
+            sets.next()
+                .map(|set| {
+                    sets.fold(set, |set1, set2| {
+                        set1.intersection(&set2)
+                            .map(|c| *c)
+                            .collect::<HashSet<char>>()
+                    })
+                })
+                .unwrap()
+                .len() as u64
+        })
+        .sum()
+}
+
+fn main() {
+    println!("{}", solve1("input.txt"));
+    println!("{}", solve2("input.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(solve1("example.txt"), 11);
+        assert_eq!(solve2("example.txt"), 6);
+    }
+}
